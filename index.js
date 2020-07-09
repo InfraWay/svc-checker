b371b3e1caa4bcdc748d97c5a38a39cc13fc2549
Andrew Red <andrew@reddikh.com>
2020-07-09 22:05:30 +0300

    - added checkers for mysql, redis connection

--- index.js
+++ index.js
@@ -1,21 +1,83 @@
 const express = require('express');
 const cors = require('cors');
 const fetch = require('node-fetch');
+const redis = require('redis');
+const mysql = require('mysql');
 
 const expressServer = express();
 const config = {
   port: process.env.PORT || 80,
+  MYSQL_HOST: process.env.MYSQL_HOST,
+  MYSQL_PORT: process.env.MYSQL_PORT,
+  MYSQL_USER: process.env.MYSQL_USER,
+  MYSQL_PASSWORD: process.env.MYSQL_PASSWORD,
+  MYSQL_DB: process.env.MYSQL_DB,
+  REDIS_HOST: process.env.REDIS_HOST,
+  REDIS_PORT: process.env.REDIS_PORT || 6379,
 };
 
-async function getExternalIP() {
+async function checkExternalIP() {
   const res = await fetch('https://api.ipify.org?format=json', {
     headers: {
       'Content-Type': 'application/json',
       'Accept': 'application/json',
-    }
+    },
   });
 
   return res.json();
+}
+
+async function checkRedis() {
+  const { REDIS_HOST: host, REDIS_PORT: port } = config;
+  if (host) {
+    return new Promise((resolve, reject) => {
+      try {
+        const client = redis.createClient(port, host);
+        client.on('error', (err) => reject({ host, port, err }));
+        client.incr('visits', (err, reply) => {
+          if (err) {
+            reject({ host, port, err });
+            return;
+          }
+          resolve({
+            host,
+            port,
+            incr: reply,
+          })
+        });
+      } catch (err) {
+        reject({ host, port, err });
+      }
+    });
+  }
+}
+
+async function checkMysql() {
+  const { MYSQL_USER: user, MYSQL_PASSWORD: password, MYSQL_HOST: host, MYSQL_PORT: port, MYSQL_DB: database } = config;
+  if (host) {
+    return new Promise((resolve, reject) => {
+      try {
+        const conn = mysql.createConnection({
+          host,
+          user,
+          password,
+          database,
+          port,
+        });
+        conn.connect();
+        conn.query('SELECT 1 + 1 AS solution', function (err, results, fields) {
+          if (err) {
+            reject({ host, port, err });
+            return;
+          }
+          resolve({ host, port, sol: results[0].solution });
+        });
+        conn.end();
+      } catch (err) {
+        reject({ host, port, err });
+      }
+    });
+  }
 }
 
 function getUserIP(req) {
@@ -29,10 +91,24 @@ function initMiddlewares(server) {
   }));
 
   server.get('/', async (req, res) => {
-    const { ip } = await getExternalIP();
+    const { ip } = await checkExternalIP();
+    let redis;
+    try {
+      redis = checkRedis();
+    } catch (e) {
+      redis = e;
+    }
+    let mysql;
+    try {
+      mysql = checkMysql();
+    } catch (e) {
+      mysql = e;
+    }
     res.json({
-      "client_ip": getUserIP(req),
-      "external_ip": ip,
+      client_ip: getUserIP(req),
+      nat_ip: ip,
+      redis,
+      mysql,
     });
   })
 }
