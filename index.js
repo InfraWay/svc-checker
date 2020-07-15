c71a569d4ab90b5d03a804d20a05811e0907cff8
Andrew Red <andrew@reddikh.com>
2020-07-15 10:58:35 +0300

    - added ability to pass mysql ssl certs for connection

--- index.js
+++ index.js
@@ -7,11 +7,16 @@ const mysql = require('mysql');
 const expressServer = express();
 const config = {
   port: process.env.PORT || 80,
+
   MYSQL_HOST: process.env.MYSQL_HOST,
   MYSQL_PORT: process.env.MYSQL_PORT || 3306,
   MYSQL_USER: process.env.MYSQL_USER,
   MYSQL_PASSWORD: process.env.MYSQL_PASSWORD,
   MYSQL_DB: process.env.MYSQL_DB,
+  MYSQL_SSL_PRIVATE: process.env.MYSQL_SSL_PRIVATE,
+  MYSQL_SSL_CERT: process.env.MYSQL_SSL_CERT,
+  MYSQL_SSL_CA: process.env.MYSQL_SSL_CA,
+
   REDIS_HOST: process.env.REDIS_HOST,
   REDIS_PORT: process.env.REDIS_PORT || 6379,
 };
@@ -56,15 +61,40 @@ async function checkRedis() {
 
 async function checkMysql() {
   return new Promise((resolve, reject) => {
-    const { MYSQL_USER: user, MYSQL_PASSWORD: password, MYSQL_HOST: host, MYSQL_PORT: port, MYSQL_DB: database } = config;
+    const {
+      MYSQL_USER: user,
+      MYSQL_PASSWORD: password,
+      MYSQL_HOST: host,
+      MYSQL_PORT: port,
+      MYSQL_DB: database,
+      MYSQL_SSL_CA: ssl_ca,
+      MYSQL_SSL_CERT: ssl_cert,
+      MYSQL_SSL_PRIVATE: ssl_private,
+    } = config;
     if (host) {
       try {
+        const getSSLConfiguration = () => {
+          if (!ssl_ca || !ssl_cert || !ssl_private) {
+            return {}
+          }
+          const encode = (data) => {
+            return (new Buffer(data, 'base64')).toString()
+          };
+          return {
+            ssl: {
+              ca: encode(ssl_ca),
+              cert: encode(ssl_cert),
+              key: encode(ssl_private)
+            }
+          }
+        }
         const conn = mysql.createConnection({
           host,
           user,
           password,
           database,
           port,
+          ...getSSLConfiguration(),
         });
         conn.connect();
         conn.query('SELECT 1 + 1 AS solution', (err, results, fields) => {
